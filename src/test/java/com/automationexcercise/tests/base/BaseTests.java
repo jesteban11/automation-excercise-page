80c0f21192eb2ebfd549ad6069aefce8cb465e39
Juanes <j.moncada@globant.com>
2023-08-01 21:49:40 -0500

    Added screenshot when the test fails

--- src/test/java/com/automationexcercise/tests/base/BaseTests.java
+++ src/test/java/com/automationexcercise/tests/base/BaseTests.java
@@ -1,11 +1,19 @@
 package com.automationexcercise.tests.base;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Allure;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class BaseTests {
@@ -25,5 +33,27 @@ public class BaseTests {
         driver.quit();
     }
 
+    @AfterMethod
+    public void recordFailure(ITestResult result) {
+        if (ITestResult.FAILURE == result.getStatus()) {
+
+            try {
+                // Directorio donde se guardarán los screenshots
+                String screenshotsDir = "target/allure-results/screenshots/";
+
+                // Capturar el screenshot
+                File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+                // Copiar el archivo de screenshot al directorio de Allure
+                FileUtils.copyFile(screenshotFile, new File(screenshotsDir + result.getName() + ".png"));
+
+                // Adjuntar el screenshot al informe de Allure
+                Allure.addAttachment(result.getName() + " - Screenshot", FileUtils.openInputStream(new File(screenshotsDir + result.getName() + ".png")));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        //System.out.println("Screenshot taken: " + screenshot.getAbsolutePath());
+    }
 
 }
