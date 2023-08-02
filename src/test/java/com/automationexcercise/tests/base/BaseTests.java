456996f43a786f39e70cbf6db13aa2a6c40761aa
Juanes <j.moncada@globant.com>
2023-08-01 21:59:18 -0500

    Added headless argument

--- src/test/java/com/automationexcercise/tests/base/BaseTests.java
+++ src/test/java/com/automationexcercise/tests/base/BaseTests.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -23,6 +24,8 @@ public class BaseTests {
     @BeforeClass
     public void setUp() {
         WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
