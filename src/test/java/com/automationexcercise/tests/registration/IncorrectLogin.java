b3476461aa8e7a48aec23b1c77a37a981f40e940
Juanes <j.moncada@globant.com>
2023-08-02 17:45:11 -0500

    Fixed test

--- src/test/java/com/automationexcercise/tests/registration/IncorrectLogin.java
+++ src/test/java/com/automationexcercise/tests/registration/IncorrectLogin.java
@@ -13,7 +13,7 @@ public class IncorrectLogin extends BaseTests {
         CreateAccount.newUser(user);
         HomePage homePage = new HomePage(driver);
         homePage.openHomePage();
-        driver.get("htttps://www.google.com.co");
+        driver.get("https://www.google.com.co");
         homePage.clickDeleteAccount();
     }
 }
